630ea340d57b02ab714e330f3cd6d1c47fff768d
Lukas_Jirak <jirak.lukas@gmail.com>
2022-03-19 23:09:27 +0100

    DB connection example.

---
+++ App/Models/ArticleModel.php
@@ -0,0 +1,17 @@
+<?php
+
+class ArticleModel
+{
+
+    private Db $db;
+
+    public function __construct()
+    {
+        $this->db = new Db();
+    }
+
+    public function getUsers(): array|bool
+    {
+        return $this->db->getAll("SELECT jmeno FROM osoba", stdClass::class);
+    }
+}
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,4 +5,9 @@
 
     <a href="{{$createLink("articles")}}">Adam je omegapepega</a>
 
+    @foreach($data as $dat)
+        <h1>{{$dat->jmeno}}</h1>
+        <h1>{{$dat->prijmeni}}</h1>
+        <p>{{$dat->popis}}</p>
+    @endforeach
 @endsection
